90e666fbd84b3a3b8c939596e40d20ec0c123a40
vcaunegre <vincent.caunegre@gmail.com>
2022-07-05 21:43:17 +0200

    first commit

---
+++ src/main/java/com/example/uploadingfile/UploadingFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploadingfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadingFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uploadingfile/UploadingFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadingfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadingFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
